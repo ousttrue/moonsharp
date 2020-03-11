d698c919c424d592ce982bc24187bea793006f34
ousttrue <ousttrue@gmail.com>
2020-03-11 14:36:22 +0900

    simplify project for vscode + .NETCore-3.1

---
+++ HelloLua/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MoonSharp.Interpreter;
+
+namespace HelloLua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string scriptCode = @"    
+		-- defines a factorial function
+		function fact (n)
+			if (n == 0) then
+				return 1
+			else
+				return n*fact(n - 1)
+			end
+		end
+
+		return fact(5)";
+
+            var script = new Script();
+            var res = script.DoString(scriptCode);
+
+            Console.WriteLine(res);
+        }
+    }
+}
